aea7878563ced89ad9f2d1fc740c666d3a0ae3ef
k883682  SR-1956 <k883682@bddordsncal-707.dordsncal>
2023-09-14 18:39:44 -0700

    add cores arg

--- 2_run_sims.R
+++ 2_run_sims.R
@@ -8,7 +8,7 @@ library(gee)
 source("0_DGP.R")
 source("1_fit_models.R")
 
-run_full_set <- function(sim_type = "complex_covars", nsims = 5, seed = NA){
+run_full_set <- function(sim_type = "complex_covars", nsims = 5, cores = 8, seed = NA){
   if(!is.na(seed)){
     set.seed(seed)
   }
@@ -26,7 +26,7 @@ run_full_set <- function(sim_type = "complex_covars", nsims = 5, seed = NA){
                         txt_eff, binary, v, sim_type)
   params
   
-  one_set <- function(DGP_specification, nsims = 5, cores = 8){
+  one_set <- function(DGP_specification, nsims = 5, cores = cores){
     
     # Get true PATE values by averaging...
     pop <- bind_rows(lapply(as.list(1:1000), function(x) gen_obs_dat(LPS_only = T,
--- 2a_run_sims_wrapper.R
+++ 2a_run_sims_wrapper.R
@@ -1,7 +1,8 @@
 source("2_run_sims.R")
 
 nsims <- 5
-results_complex_covars <- run_full_set(sim_type = "complex_covars", nsims = nsims, seed = NA)
+results_complex_covars <- run_full_set(sim_type = "complex_covars",
+                                       nsims = nsims, seed = NA)
 saveRDS(results_complex_covars, "results/results_complex_covars.rds")
 
 
